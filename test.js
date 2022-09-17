a7291cca09ea90ef7231733c0b0c7dd3c16c0583
jinnyl22 <smile2374@gmail.com>
2022-09-17 21:55:17 +0900

    나머지가 1이 되는 것 중에 제일 작은 수

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-function solution(seoul) {
-  let a = seoul.indexOf("Kim");
-  return `김서방은 ${a}에 있다`;
+// 나머지가 1이 되게하는 수 중에 가장 작은 수
+// x로 나눔
+function solution(n) {
+  for (let x = 2; x < n; x++) {
+    if (n % x == 1) return x;
+  }
 }
-// 테스트 코드입니당
-console.log(solution(["j", "r", "s", "Kim"]));
+console.log(solution(11));

aee8e8026da1349a97d06786a040b0f417a8e6cf
jinnyl22 <smile2374@gmail.com>
2022-10-08 19:52:16 +0900

    세균증식

--- test2.js
+++ test2.js
@@ -1,8 +1,8 @@
-// 양꼬치
-function solution(n, k) {
-  var a = n * 12000;
-  var b = k * 2000;
-  var c = Math.floor(n / 10) * 2000;
-  return a + b - c;
+// 세균 증식
+// 1시간에 두배만큼 증식
+function solution(n, t) {
+  for (let i = 1; i <= t; i++) {
+    n = n * 2;
+  }
+  return n;
 }
-console.log(solution(15, 2));

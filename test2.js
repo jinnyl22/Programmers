cb5d03d348d226fe3a6daa0f0e38e67d11e7e6d4
jinnyl22 <smile2374@gmail.com>
2022-11-01 17:45:37 +0900

    짝수의 합

--- test2.js
+++ test2.js
@@ -1,9 +1,10 @@
-// 각도기
-function solution(angle) {
-  if (0 < angle && angle < 90) return 1;
-  else if (angle == 90) return 2;
-  else if (90 < angle && angle < 180) return 3;
-  else return 4;
+// 짝수의 합
+function solution(n) {
+  let answer = 0;
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 == 0) answer += i;
+  }
+  return answer;
 }
 
-console.log(solution(90));
+console.log(solution(4));

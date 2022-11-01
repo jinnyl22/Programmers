ca5c03287948945367f266b82e9a101ac3767a1d
jinnyl22 <smile2374@gmail.com>
2022-11-01 17:22:45 +0900

    각도기

--- test2.js
+++ test2.js
@@ -1,6 +1,9 @@
-// 나이 출력
-const solution = (age) => {
-  return (answer = 2022 - age + 1);
-};
+// 각도기
+function solution(angle) {
+  if (0 < angle && angle < 90) return 1;
+  else if (angle == 90) return 2;
+  else if (90 < angle && angle < 180) return 3;
+  else return 4;
+}
 
-console.log(solution(40));
+console.log(solution(90));

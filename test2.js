89a6ffdb2cebf32a8a82940cbb8190512ee3146a
jinnyl22 <smile2374@gmail.com>
2022-11-19 22:43:51 +0900

    문자열 뒤집기

--- test2.js
+++ test2.js
@@ -1,6 +1,11 @@
-// 배열 뒤집기
-function solution(num_list) {
-  return (answer = num_list.reverse());
+// 문자열 뒤집기
+function solution(my_string) {
+  let str = [];
+  for (let i = 1; i <= my_string.length; i++) {
+    str.push(my_string.substring(i - 1, i));
+  }
+  let answer = str.reverse();
+  return answer.join("");
 }
 
-// console.log(solution([1, 2, 3]));
+// console.log(solution("jaron"));

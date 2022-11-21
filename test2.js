06a81f72ce45d334351e15f34b13b968cdb62017
jinnyl22 <smile2374@gmail.com>
2022-11-21 10:01:24 +0900

    피자나눠먹기

--- test2.js
+++ test2.js
@@ -1,11 +1,6 @@
-// 문자열 뒤집기
-function solution(my_string) {
-  let str = [];
-  for (let i = 1; i <= my_string.length; i++) {
-    str.push(my_string.substring(i - 1, i));
-  }
-  let answer = str.reverse();
-  return answer.join("");
+// 피자 나눠먹기
+function solution(slice, n) {
+  return n % slice == 0 ? n / slice : parseInt(n / slice) + 1;
 }
 
-// console.log(solution("jaron"));
+// console.log(solution(7, 10));

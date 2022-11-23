af372cb56346f74c43995a4bde64d1276af04ae1
jinnyl22 <smile2374@gmail.com>
2022-11-23 10:16:34 +0900

    배열 자르기

--- test2.js
+++ test2.js
@@ -1,12 +1,6 @@
-// n의 배수 고르기
-function solution(n, numlist) {
-  const answer = [];
-  numlist.map((el) => {
-    if (el % n == 0) {
-      answer.push(el);
-    }
-  });
-  return answer;
+// 배열 자르기
+function solution(numbers, num1, num2) {
+  return (answer = numbers.slice(num1, num2 + 1));
 }
 
-// console.log(solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12, 13]));
+console.log(solution([1, 2, 3, 4, 5], 1, 3));
